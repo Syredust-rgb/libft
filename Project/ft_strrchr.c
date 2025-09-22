b4b1766bda3e6369d09b6835b5e014e7cbe97445
Syredust-rgb <iturraimar2007@gmail.com>
2025-09-22 09:54:27 +0200

    ft_strrchr

---
+++ Project/ft_strrchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aiturral <aiturral@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/22 09:45:31 by aiturral          #+#    #+#             */
+/*   Updated: 2025/09/22 09:47:19 by aiturral         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+char	*ft_strrchr(const char *str, int chr)
+{
+	int		i;
+	char	*last;
+
+	i = 0;
+	last = NULL;
+	while (str[i] != '\0')
+	{
+		if (str[i] == (char)chr)
+			last = (char *)&str[i];
+		i++;
+	}
+	if ((char)chr == '\0')
+		return ((char *)&str[i]);
+	return (last);
+}
