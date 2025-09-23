e368e2c7c8128cdee9d0fc20ac27549516b54ba0
Syredust-rgb <iturraimar2007@gmail.com>
2025-09-23 17:54:08 +0200

    fix norm

--- Project/ft_memchr.c
+++ Project/ft_memchr.c
@@ -6,17 +6,17 @@
 /*   By: aiturral <aiturral@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 17:33:07 by aiturral          #+#    #+#             */
-/*   Updated: 2025/09/23 17:44:08 by aiturral         ###   ########.fr       */
+/*   Updated: 2025/09/23 17:52:28 by aiturral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-void *memchr(const void *s, int c, size_t n)
+void	*memchr(const void *s, int c, size_t n)
 {
-	unsigned char *ptr;
-	unsigned char target;
-	size_t it;
+	unsigned char	*ptr;
+	unsigned char	target;
+	size_t			it;
 
 	ptr = (unsigned char *)s;
 	target = (unsigned char)c;
@@ -24,9 +24,8 @@ void *memchr(const void *s, int c, size_t n)
 	while (it < n)
 	{
 		if (ptr[it] == target)
-			return (void *)(ptr + it);
+			return ((void *)(ptr + it));
 		it++;
 	}
 	return (NULL);
 }
-
--- Project/ft_strncmp.c
+++ Project/ft_strncmp.c
@@ -6,22 +6,20 @@
 /*   By: aiturral <aiturral@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 10:59:30 by aiturral          #+#    #+#             */
-/*   Updated: 2025/09/23 10:59:32 by aiturral         ###   ########.fr       */
+/*   Updated: 2025/09/23 17:49:43 by aiturral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include <stddef.h>
 
-int	ft_strncmp(const char * str1, const char * str2, size_t n)
+int	ft_strncmp(const char *str1, const char *str2, size_t n)
 {
-	size_t it;
+	size_t	it;
 
 	it = 0;
 	while (i < n && s1[it] && s2[it] && s1[it] == s2[it])
 		it++;
 	if (it == n)
-        return 0;
-    return (unsigned char)s1[it] - (unsigned char)s2[it];
+		return (0);
+	return ((unsigned char)s1[it] - (unsigned char)s2[it]);
 }
-
