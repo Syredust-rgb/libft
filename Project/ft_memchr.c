1a349540d7ca8e5e336f783264c7198ca25aa7e2
Syredust-rgb <iturraimar2007@gmail.com>
2025-09-23 17:44:38 +0200

    ft_memchr

---
+++ Project/ft_memchr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aiturral <aiturral@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/23 17:33:07 by aiturral          #+#    #+#             */
+/*   Updated: 2025/09/23 17:44:08 by aiturral         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void *memchr(const void *s, int c, size_t n)
+{
+	unsigned char *ptr;
+	unsigned char target;
+	size_t it;
+
+	ptr = (unsigned char *)s;
+	target = (unsigned char)c;
+	it = 0;
+	while (it < n)
+	{
+		if (ptr[it] == target)
+			return (void *)(ptr + it);
+		it++;
+	}
+	return (NULL);
+}
+
