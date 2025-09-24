cef5f72898e87302054e9909b6fe361369eb137c
Syredust-rgb <iturraimar2007@gmail.com>
2025-09-24 17:13:26 +0200

    ft_atoi

---
+++ Project/ft_atoi.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aiturral <aiturral@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/24 17:02:34 by aiturral          #+#    #+#             */
+/*   Updated: 2025/09/24 17:12:07 by aiturral         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	is_space(char c)
+{
+	return (c == ' ' || c == '\t'
+		|| c == '\n' || c == '\v'
+		|| c == '\f' || c == '\r');
+}
+
+int	ft_atoi(const char *str)
+{
+	int	result;
+	int	sign;
+
+	result = 0;
+	sign = 1;
+	while (*str && is_space(*str))
+		str++;
+	if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	else if (*str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	return (sign * result);
+}
