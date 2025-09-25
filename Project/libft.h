8193c1580aa46578a47ad138363780276b626167
Syredust-rgb <iturraimar2007@gmail.com>
2025-09-25 12:57:51 +0200

    libft.h

---
+++ Project/libft.h
@@ -0,0 +1,28 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+
+int	ft_atoi(const char *str);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t num, size_t size);
+int	ft_isalnum (int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_digit(int c);
+int	ft_isprint(int c);
+void	*memchr(const void *s, int c, size_t n);
+int	ft_memcmp(void *pointer1, void *pointer2, size_t size);
+void	*ft_memcpy(void *dest, const void *src, size_t count);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
+char	*ft_strchr(const char *str, int chr);
+char	*my_strdup (const char *s);
+size_t	ft_strlcat(char *dest, char const *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *s);
+int	ft_strncmp(const char *str1, const char *str2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strrchr(const char *str, int chr);
+int	ft_lower(int c);
+int	ft_toupper(int c);
+
+#endif /* LIBFT_H */
