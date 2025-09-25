0e53f3821ad753e30e4566ad9bd8e232d79c4a25
Syredust-rgb <iturraimar2007@gmail.com>
2025-09-25 19:19:01 +0200

    ft_strtrim

---
+++ Project/ft_strtrim.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aiturral <aiturral@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/25 18:07:41 by aiturral          #+#    #+#             */
+/*   Updated: 2025/09/25 18:27:51 by aiturral         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stddef.h>
+
+int	is_in_set(char c, const char *set)
+{
+	while (*set)
+	{
+		if (c == *set)
+			return (1);
+		set++;
+	}
+	return (0);
+}
+
+char	*ft_strncpy(char *dest, const char *src, size_t n)
+{
+	size_t	it;
+
+	it = 0;
+	while (it < n && src[it])
+	{
+		dest[it] = src[it];
+		it++;
+	}
+	while (it < n)
+	{
+		dest[it] = '\0';
+		it++;
+	}
+	return (dest);
+}
+
+char	*ft_strtrim(const char *s1, const char *set)
+{
+	size_t	start;
+	size_t	end;
+	size_t	len;
+	char	*trimmed;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	while (s1[start] && is_in_set(s1[start], set))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && is_in_set(s1[end - 1], set))
+		end--;
+	len = end - start;
+	trimmed = (char *)malloc(len + 1);
+	if (!trimmed)
+		return (NULL);
+	ft_strncpy(trimmed, s1 + start, len);
+	trimmed[len] = '\0';
+	return (trimmed);
+}
