124062fd1f2e8ba9ffa2008e9e9fc45a21388a5b
Syredust-rgb <iturraimar2007@gmail.com>
2025-09-25 12:01:57 +0200

    ft_strdup

---
+++ Project/ft_strdup.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aiturral <aiturral@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/25 11:48:14 by aiturral          #+#    #+#             */
+/*   Updated: 2025/09/25 12:01:33 by aiturral         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include <stdlib.h>
+
+size_t	ft_strlen (const char *str)
+{
+	size_t it;
+
+	it = 0;
+	while (str[it])
+		it++;
+	return (it);
+}
+
+char	*ft_strcpy (char *dest, const char *src)
+{
+	char *ptr;
+
+	ptr = dest;
+	while (*src)
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
+	*ptr = '\0';
+	return (dest);
+}
+
+char	*my_strdup (const char *s)
+{
+	size_t len;
+	char *copy;
+
+	if (s == NULL)
+		return (NULL);
+	len = ft_strlen(s);
+	copy = malloc(len + 1);
+	if (copy == NULL)
+		return (NULL);
+	ft_strcpy (copy, s);
+	return (copy);
+}
