2bec9e002322f6cb0a58025953ee7493afa62b9a
Syredust-rgb <iturraimar2007@gmail.com>
2025-09-25 17:27:59 +0200

    ft_substr.c

---
+++ Project/ft_substr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aiturral <aiturral@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/25 16:15:23 by aiturral          #+#    #+#             */
+/*   Updated: 2025/09/25 16:35:18 by aiturral         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stddef.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	str_len;
+	size_t	it;
+
+	if (!s)
+		return (NULL);
+	str_len = ft_strlen(s);
+	if (start >= str_len)
+		return (ft_strdup(""));
+	if (len > str_len - start)
+		len = str_len - start;
+	substr = (char *)malloc(sizeof(char) * (len + 1));
+	if (!substr)
+		return (NULL);
+	it = 0;
+	while (it < len && s[start + it])
+	{
+		substr[it] = s[start + it];
+		it++;
+	}
+	substr[it] = '\0';
+	return (substr);
+}
