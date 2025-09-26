d7a333ea1a052f2c459eb67793b9766f721133c1
Syredust-rgb <iturraimar2007@gmail.com>
2025-09-26 08:49:59 +0200

    ft_itoa

---
+++ Project/ft_itoa.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aiturral <aiturral@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/26 08:28:10 by aiturral          #+#    #+#             */
+/*   Updated: 2025/09/26 08:45:45 by aiturral         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_num_len(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n <= 0)
+		len = 1;
+	while (n != 0)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+void	ft_handle_negative(long *nbr, char *str)
+{
+	if (*nbr < 0)
+	{
+		str[0] = '-';
+		*nbr = -(*nbr);
+	}
+}
+
+void	ft_convert_number(long nbr, char *str, int len)
+{
+	if (nbr == 0)
+		str[0] = '0';
+	while (nbr > 0)
+	{
+		str[--len] = (nbr % 10) + '0';
+		nbr /= 10;
+	}
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	long	nbr;
+	int		len;
+
+	nbr = n;
+	len = ft_num_len(n);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	ft_handle_negative(&nbr, str);
+	ft_convert_number(nbr, str, len);
+	return (str);
+}
--- Project/ft_libft.h
+++ Project/ft_libft.h
@@ -40,5 +40,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(const char *s1, const char *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 
 #endif /* LIBFT_H */
