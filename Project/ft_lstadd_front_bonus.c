9a935c4189c182b32d47108e88a5cbdf9b972af3
Syredust-rgb <iturraimar2007@gmail.com>
2025-10-05 17:32:06 +0200

    FINAL FIX (I FUCKING HOPE)

--- Project/ft_lstadd_front_bonus.c
+++ Project/ft_lstadd_front_bonus.c
@@ -17,5 +17,5 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 	if (!new)
 		return ;
 	new -> next = *lst;
-	lst = new;
+	*lst = new;
 }
--- Project/libft.h
+++ Project/libft.h
@@ -6,7 +6,7 @@
 /*   By: aiturral <aiturral@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 13:00:11 by aiturral          #+#    #+#             */
-/*   Updated: 2025/10/05 16:48:24 by aiturral         ###   ########.fr       */
+/*   Updated: 2025/10/05 17:01:57 by aiturral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,6 @@
 # include <stdlib.h>
 # include <stdint.h>
 # include <unistd.h>
-
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}			t_list;
 
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
@@ -58,6 +52,12 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}						t_list;
+
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
@@ -67,6 +67,6 @@ void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 
-t_list	*ft_lstmap(t_list *lst,	void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
