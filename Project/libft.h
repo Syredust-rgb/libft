abdfe2db41c149baea5106f50477f7be29e19f84
Syredust-rgb <iturraimar2007@gmail.com>
2025-10-05 16:20:09 +0200

    ft_lstdelone

--- Project/libft.h
+++ Project/libft.h
@@ -6,7 +6,7 @@
 /*   By: aiturral <aiturral@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 13:00:11 by aiturral          #+#    #+#             */
-/*   Updated: 2025/10/02 11:37:45 by aiturral         ###   ########.fr       */
+/*   Updated: 2025/10/05 16:19:42 by aiturral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,10 +57,13 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void
+*));
 
 #endif
