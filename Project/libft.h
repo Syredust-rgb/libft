0aaba05a60f3fee866f4c1f3b253fe1dda010b68
Syredust-rgb <iturraimar2007@gmail.com>
2025-10-05 16:49:18 +0200

    Update libft.h

--- Project/libft.h
+++ Project/libft.h
@@ -6,7 +6,7 @@
 /*   By: aiturral <aiturral@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 13:00:11 by aiturral          #+#    #+#             */
-/*   Updated: 2025/10/05 16:19:42 by aiturral         ###   ########.fr       */
+/*   Updated: 2025/10/05 16:48:24 by aiturral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,10 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
-void	ft_lstdelone(t_list *lst, void (*del)(void
-*));
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+
+t_list	*ft_lstmap(t_list *lst,	void *(*f)(void *), void (*del)(void *))
 
 #endif
