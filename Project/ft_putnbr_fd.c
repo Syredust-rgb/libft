0e546795cda448e1956db3986f5b89ef4dc557ea
John Doe <johndoe@example.com>
2025-10-07 16:39:22 +0200

    puta vida

--- Project/ft_putnbr_fd.c
+++ Project/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: aiturral <aiturral@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 10:18:08 by aiturral          #+#    #+#             */
-/*   Updated: 2025/09/30 19:05:31 by aiturral         ###   ########.fr       */
+/*   Updated: 2025/10/07 16:38:16 by aiturral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,11 @@ void	ft_putnbr_fd(int n, int fd)
 	char	c;
 
 	if (n == -2147483648)
+	{
 		write(fd, "-2147483648", 11);
-	else if (n < 0)
+		return ;
+	}
+	if (n < 0)
 	{
 		write(fd, "-", 1);
 		n = -n;
@@ -27,4 +30,10 @@ void	ft_putnbr_fd(int n, int fd)
 		ft_putnbr_fd(n / 10, fd);
 	c = (n % 10) + '0';
 	write(fd, &c, 1);
+}
+
+int main (void)
+{
+	#include <stdio.h>
+	ft_putnbr_fd(INT32_MIN, 1);
 }
